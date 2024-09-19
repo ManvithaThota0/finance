a1880838c63a9a0d8b527d5921b552b40653ce6f
manvitha thota <ManvithaThota0@users.noreply.github.com>
2024-09-19 00:31:17 -0400

    Spring Framework is created

---
+++ SpringFramework/myfirst-spring-framework 2/src/main/java/com/myproject/myfirst_spring_framework/MyfirstSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.myfirst_spring_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ SpringFramework/myfirst-spring-framework 2/src/test/java/com/myproject/myfirst_spring_framework/MyfirstSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.myfirst_spring_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
